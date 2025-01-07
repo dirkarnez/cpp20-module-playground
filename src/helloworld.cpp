7b9a6d22c41865eed1ff0b5badc1b78c8ab98260
dirkarnez <smalldirkalex@gmail.com>
2025-01-07 18:55:02 +0800

    - working

---
+++ src/helloworld.cpp
@@ -0,0 +1,13 @@
+export module helloworld; // module declaration
+ 
+// import <iostream>;        // import declaration
+ 
+export int sum(int a, int b)       // export declaration
+{
+    return a + b;
+}
+
+// export void say_hello()       // export declaration
+// {
+//     std::cout << "Hello world!\n";
+// }
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// main.cpp
+import helloworld; // import declaration
+
+#include <iostream>
+
+int main()
+{
+    // say_hello();
+    int a = 9;
+    int b = 15;
+    std::cout << a << " + " << b << " = " << sum(a, b) << std::endl;
+}
